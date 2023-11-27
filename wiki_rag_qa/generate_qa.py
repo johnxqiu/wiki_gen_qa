b271510241f6b5192cd6b41652d77ae8b0ee236a
johnxqiu <{ID}+{username}@users.noreply.github.com>
2023-11-26 22:17:54 -0500

    initial commit

---
+++ wiki_rag_qa/generate_qa.py
@@ -0,0 +1,41 @@
+from typing import List
+
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    SystemMessagePromptTemplate,
+)
+from langchain_core.language_models.chat_models import BaseChatModel
+
+FACT_SPLIT_TEMPLATE = "Extract the individual facts out of this text. Don't include opinions. Return each claim on a new line with no header and keep them short sentences.\n"
+INPUT_QUERY_TEMPLATE = "{query}"
+QUESTION_GENERATION_TEMPLATE = "Given the following claim about {page_title}, generate a single sentence question that comprehensively answers the following statement. \n"
+
+fact_split_instruction_template = SystemMessagePromptTemplate.from_template(
+    FACT_SPLIT_TEMPLATE
+)
+query_template = HumanMessagePromptTemplate.from_template(INPUT_QUERY_TEMPLATE)
+
+
+def get_facts_from_summary(in_query: str, in_chat: BaseChatModel) -> List[str]:
+    fact_splitting_prompt = ChatPromptTemplate.from_messages(
+        [FACT_SPLIT_TEMPLATE, query_template]
+    )
+    response = in_chat(
+        fact_splitting_prompt.format_prompt(query=in_query).to_messages()
+    )
+    return response.content.split("\n")
+
+
+def get_question_from_sentence(
+    in_page_title: str, in_test_sentence: str, in_chat: BaseChatModel
+) -> str:
+    question_generating_prompt = ChatPromptTemplate.from_messages(
+        [QUESTION_GENERATION_TEMPLATE, INPUT_QUERY_TEMPLATE]
+    )
+    response = in_chat(
+        question_generating_prompt.format_prompt(
+            page_title=in_page_title, query=in_test_sentence
+        ).to_messages()
+    )
+    return response.content
---
+++ wiki_rag_qa/wiki_tools.py
@@ -0,0 +1,41 @@
+"""code for getting data from using wikipedia-api
+"""
+from typing import List
+
+import nltk
+import wikipediaapi
+from nltk.tokenize import sent_tokenize
+
+USER_AGENT = "MyProjectName (merlin@example.com)"
+nltk.download("punkt")
+
+
+def try_get_wikipedia_page(in_page_name: str) -> wikipediaapi.WikipediaPage:
+    try:
+        wiki_wiki = wikipediaapi.Wikipedia(
+            user_agent=USER_AGENT,
+            language="en",
+            extract_format=wikipediaapi.ExtractFormat.WIKI,
+        )
+        return wiki_wiki.page(in_page_name)
+
+    except wikipediaapi.exceptions.DisambiguationError:
+        raise wikipediaapi.exceptions.DisambiguationError
+
+    except wikipediaapi.exceptions.PageError:
+        raise wikipediaapi.exceptions.PageError
+
+
+def get_wikipedia_summary_sentences(in_page_name: str) -> List[str]:
+    wiki_page = try_get_wikipedia_page(in_page_name)
+    summary_sentences = sent_tokenize(wiki_page.summary)
+    return summary_sentences
+
+
+def get_wikipedia_content(in_page_name: str) -> str:
+    wiki_page = try_get_wikipedia_page(in_page_name)
+    sections = wiki_page.sections
+    out_content = ""
+    for section in sections:
+        out_content += section.title + "\n" + section.text + "\n"
+    return out_content
